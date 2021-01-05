4e62f578c7e548ab1a7a7445177614ba3d372b5f
Lea Krautheimer <limk98@gmx.de>
2021-01-05 10:04:31 +0100

    please

---
+++ b1a3.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include <math.h>
+
+double function(double x);
+double function(double x){
+	return tanh(x);
+}
+
+double dfunction(double x);
+double dfunction(double x){
+	return 1 / (cosh(x) * cosh(x));
+}
+
+int main(){
+	double x_new, x_old, seed;
+	double epsilon = 1e-8;
+	int count = 0;
+	
+	seed = 0.5;
+	x_new = seed;
+	
+	do{
+		x_old = x_new;
+		x_new = x_old - function(x_old) / dfunction(x_old);
+		count++;
+	} while (fabs(x_new - x_old) > epsilon && count < 100000);
+	
+	printf("%f\n", x_new);
+	
+	return 0;
+}
